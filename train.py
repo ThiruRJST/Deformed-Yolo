1ce7422f0a686517576bf25792af72e30482a54d
ThiruRJST <maroonbleed007@gmail.com>
2021-09-04 08:39:54 +0530

    Training and Val Loops ready

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ import os
 import numpy as np
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
+from callbacks import EarlyStopping
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 import cv2
@@ -31,6 +32,7 @@ DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 TOTAL_EPOCHS = 100
 scaler = GradScaler()
 writer = SummaryWriter()
+early_stop = EarlyStopping()
 
 
 
@@ -42,7 +44,8 @@ print("Model Shipped to {}".format(DEVICE))
 
 data = pd.read_csv("data.csv")
 
-loss_fn = nn.BCEWithLogitsLoss()
+train_loss_fn = nn.BCEWithLogitsLoss()
+val_loss_fn = nn.BCEWithLogitsLoss()
 
 optim = torch.optim.Adam(Model.parameters())
 
@@ -76,6 +79,7 @@ class dog_cat(Dataset):
 
 
 def train_loop(epoch,dataloader,model,loss_fn,optim,device=DEVICE):
+    model.train()
     epoch_loss = 0
     epoch_acc = 0
     #start_time = time.time()
@@ -104,16 +108,57 @@ def train_loop(epoch,dataloader,model,loss_fn,optim,device=DEVICE):
         epoch_loss += train_loss.item()
         epoch_acc += correct.item() / out.shape[0]
 
-    writer.add_scalar("Training_Loss",epoch_loss/len(dataloader),epoch)
-    writer.add_scalar("Training_Acc",epoch_acc/len(dataloader),epoch)
+    train_epoch_loss = epoch_loss / len(dataloader)
+    train_epoch_acc = epoch_acc / len(dataloader)
+
+    writer.add_scalar("Training_Loss",train_epoch_loss,epoch)
+    writer.add_scalar("Training_Acc",train_epoch_acc,epoch)
         
-    print(f"Epoch:{epoch}/{TOTAL_EPOCHS} Epoch Loss:{epoch_loss / len(dataloader):.4f} Epoch Acc:{epoch_acc / len(dataloader):.4f}")
+    #print(f"Epoch:{epoch}/{TOTAL_EPOCHS} Epoch Loss:{epoch_loss / len(dataloader):.4f} Epoch Acc:{epoch_acc / len(dataloader):.4f}")
+    
+    return train_epoch_loss,train_epoch_acc
+
+def val_loop(epoch,dataloader,model,loss_fn,device = DEVICE):
+    model.eval()
+    val_epoch_loss = 0
+    val_epoch_acc = 0
+    pbar = tqdm(enumerate(dataloader))
+
+    with torch.no_grad():
+        for i,(img,label) in pbar:
+            img = img.to(device)
+            label = label.to(device)
+
+            yhat = model(img).flatten()
+            val_loss = loss_fn(input=yhat,target=label)
+
+            out = (yhat>0.5).float()
+            correct = (yhat == label).float().sum()
+
+            val_epoch_loss += val_loss.item()
+            val_epoch_acc += correct.item() / out.shape[0]
+
+        val_lossd = val_epoch_loss / len(dataloader)
+        val_accd = val_epoch_acc / len(dataloader)
+        
+        writer.add_scalar("Val_Loss",val_lossd,epoch)
+        writer.add_scalar("Val_Acc",val_accd/len(dataloader),epoch)
+
+        return val_lossd,val_accd
+
+
+
+
+
+
         
     
 
 
 if __name__ == "__main__":
 
+    train_per_epoch_loss,train_per_epoch_acc = [],[]
+    val_per_epoch_loss,val_per_epoch_acc = [],[]
     train = dog_cat(data,transforms=Compose([Resize(256,256),ToTensorV2()]))
     val = dog_cat(data,mode='val',transforms=Compose([Resize(256,256),ToTensorV2()]))
 
@@ -121,7 +166,21 @@ if __name__ == "__main__":
     val_load = DataLoader(val,batch_size=16,num_workers=4)
 
     for e in range(TOTAL_EPOCHS):
-        train_loop(e,train_load,Model,loss_fn,optim)
+        train_loss,train_acc = train_loop(e,train_load,Model,train_loss_fn,optim)
+        val_loss,val_acc = val_loop(e,val_load,Model,val_loss_fn)
+        train_per_epoch_loss.append(train_loss)
+        train_per_epoch_acc.append(train_acc)
+        val_per_epoch_loss.append(val_loss)
+        val_per_epoch_acc.append(val_acc)
+        early_stop(val_loss)
+        if early_stop.early_stop:
+            break
+
+
+        
+
+
+
 
 
 
